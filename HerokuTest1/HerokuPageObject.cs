13c01f6f87177b6b6e7f0bd6ab8f3b138d9873a4
Keith <keith@beatquestgames.com>
2023-01-04 15:01:46 -0500

    Add project files.

---
+++ HerokuTest1/HerokuPageObject.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HerokuTest1
+{
+    class HerokuPageObject
+    {
+        public String StartingUrl = "https://the-internet.herokuapp.com/add_remove_elements/";
+        private IWebDriver driver;
+
+        public HerokuPageObject(IWebDriver driver)
+        {
+            this.driver = driver; 
+            this.driver.Url = StartingUrl; 
+
+
+        }
+        public IWebElement AddElementButton()
+        {
+            return this.driver.FindElement(By.CssSelector("[onclick=\"addElement()\"]"));
+
+
+        }
+    }
+}
---
+++ HerokuTest1/SeleniumSetUp.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace HerokuTest1
+{
+    class SeleniumSetUp
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            string currentDirectory = Directory.GetCurrentDirectory(); 
+            driver = new ChromeDriver(Path.Combine(currentDirectory,"chromedriver_win32\\chromedriver.exe")); 
+        }
+
+        [Test]
+        public void test()
+        {
+            driver.Url = "http://www.google.com";
+            Console.WriteLine(driver.CurrentWindowHandle); 
+        }
+        [Test]
+        public void test2()
+        {
+            driver.Url = "http://www.google.co.in";
+            Console.WriteLine(driver.CurrentWindowHandle);
+        }
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+
+    }
+}
